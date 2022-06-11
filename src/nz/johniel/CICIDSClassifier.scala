e8f418c01ac72167401389444ea7dd073b8f1d6c
Johniel Bocacao <accounts@johniel.nz>
2022-06-11 14:43:20 +1200

    Stratify train/test split, make it multiclass

--- src/nz/johniel/CICIDSClassifier.scala
+++ src/nz/johniel/CICIDSClassifier.scala
@@ -1,14 +1,11 @@
 package nz.johniel
 
-import org.apache.hadoop.fs.FileSystem
-import org.apache.hadoop.fs.Path
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, RegressionEvaluator}
-import org.apache.spark.ml.{Pipeline, PipelineStage}
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{StringIndexer, VectorAssembler}
-import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
  * @author Johniel Bocacao (300490028)
@@ -33,7 +30,8 @@ object CICIDSClassifier extends App {
   val loadType = args(0)
   val conf = session.sparkContext.hadoopConfiguration
   var fs : FileSystem = _
-  var path : String = "input/"
+  var path = "input/"
+  val verbose = true
   if (loadType == "hdfs") {
     fs = FileSystem.get(conf)
   } else if (loadType == "local") {
@@ -53,59 +51,62 @@ object CICIDSClassifier extends App {
     val friday = readCSV(path + "Friday-WorkingHours-Morning.pcap_ISCX.csv") // Join separated files together
       .union(readCSV(path + "Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv"))
       .union(readCSV(path + "Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv"))
-    println("Raw data nrows: " + friday.count())
     // Drop NAs
     val fridayClean = friday.na.drop()
-    println("Cleaned data nrows: " + fridayClean.count())
+    if (verbose) {
+      println("Raw data nrows: " + friday.count())
+      println("Cleaned data nrows: " + fridayClean.count())
+    }
 
-    // 1. Split test set off first thing
-    val Array(train, test) = fridayClean.randomSplit(Array(0.7, 0.3), seed = 0)
+    // 1. Split test set off first thing ============================================
+    // Stratify sampling by label, get 50% from each class
+    val train = fridayClean.stat.sampleBy(" Label", Map("BENIGN" -> 0.5, "PortScan" -> 0.5, "Bot" -> 0.5, "DDoS" -> 0.5), 0)
+    // Get the test set by getting the set exception
+    val test = fridayClean.exceptAll(train)
+    if (verbose) {
+      train.groupBy(" Label").count().show()
+      println("Train nrows: " + train.count())
+      test.groupBy(" Label").count().show()
+      println("Test nrows: " + test.count())
+    }
 
-    // 2. Define and build pipeline
+    // 2. Define and build pipeline ============================================
     val featureCols = train.columns.filter(x => x != " Label")
-    featureCols.mkString(",")
-    val assembler = new VectorAssembler()
-      .setInputCols(featureCols)
-      .setOutputCol("features")
-    val indexer = new StringIndexer()
-      .setInputCol(" Label")
-      .setOutputCol("label")
-    val lr = new LogisticRegression()
-      .setMaxIter(20)
-      .setRegParam(0.01)
-      .setElasticNetParam(0.1)
+    if (verbose) println("Features: " + featureCols.mkString(", "))
+
     val pipeline = new Pipeline()
-      .setStages(Array(assembler, indexer, lr))
+      .setStages(Array(
+        // Group features into a vector
+        new VectorAssembler()
+          .setInputCols(featureCols)
+          .setOutputCol("features"),
+        // Index label strings as integers
+        new StringIndexer()
+          .setInputCol(" Label")
+          .setOutputCol("label"),
+        // Classifier as final step
+        new DecisionTreeClassifier()
+      ))
     val model = pipeline.fit(train)
 
-    // 3. Test pipeline
-    val predictions = model.transform(test)
+    // 3. Test pipeline on train data ============================================
+    println("> Training metrics (" + train.count() + " instances)")
+    testPipeline(model, train)
 
-    val evaluator = new BinaryClassificationEvaluator()
-      .setLabelCol("label")
-      .setRawPredictionCol("prediction")
-      .setMetricName("areaUnderROC")
-    println("Accuracy (AU-ROC): " + evaluator.evaluate(predictions))
-//
-//    // 3. Train pipeline with cross validation
-//    val grid = new ParamGridBuilder()
-//      .addGrid(lr.fitIntercept)
-//      .addGrid(lr.regParam, Array(0.0001, 0.001, 0.01, 0.1, 0.5, 1))
-//      .addGrid(lr.elasticNetParam, Array(0, 1, 0.01))
-//      .build()
-//    val cv = new TrainValidationSplit()
-//      .setEstimator(pipeline)
-//      .setEvaluator(new BinaryClassificationEvaluator())
-//      .setEstimatorParamMaps(grid)
-//      .setTrainRatio(0.3)
-//    val model = cv.fit(train)
-//
-//    // 4. Test pipeline
-//    model.transform(test)
-//      .select("features", "label", "prediction")
-//      .show()
+    // 4. Test pipeline on test data ============================================
+    println("> Test metrics(" + test.count() + " instances)")
+    testPipeline(model, test)
   } else {
     println("ERROR: 3 input/Friday-WorkingHours-*.csv's not found in data directory or HDFS input")
+  }
+
+  private def testPipeline(model : PipelineModel, df : DataFrame): Unit = {
+    val predictions = model.transform(df)
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+    println("Accuracy = " + accuracy)
   }
 
   private def readCSV(filePath: String) = {
